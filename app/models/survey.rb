1fcac6b81da05da026c262ec426cf1807cc79e7d
Jarrett Green <himself@jarrettgreen.com>
2025-04-16 14:48:05 -0500

    Validate presence of title on surveys

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,6 +1,7 @@
 class Survey < ApplicationRecord
   has_many :questions, dependent: :destroy
   has_many :responses, dependent: :destroy
+  validates :title, presence: true
 
   ROLES = ['data_engineer', 'frontend_engineer', 'product_manager'].freeze
 
